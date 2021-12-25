ed3babb81ca6cc627547a9fd6a93f73cd27b01ba
jpiop <jpiopadilla@gmail.com>
2021-12-25 19:45:21 +0000

    Arreglada clase File

--- src/File.cc
+++ src/File.cc
@@ -2,28 +2,37 @@
 
 File::File(std::string pathname, int flags) {
   fd_ = open(pathname.c_str(), flags);
+  if (fd_ < 0) {
+    throw std::system_error(errno, std::system_category(),
+                            "Error al abrir el archivo.");
+  }
   struct stat st {};
   fstat(fd_, &st);
   size_ = st.st_size;
-  mem_start_ = (char*)mmap(NULL, size_, PROT_WRITE, MAP_SHARED, fd_, 0);
+  mem_start_ = static_cast<uint8_t*>(
+      mmap(nullptr, size_ * sizeof(uint8_t), PROT_READ, MAP_SHARED, fd_, 0));
+
   lockf(fd_, F_LOCK, 0);
 }
 
 ssize_t File::read(void* buf, size_t size) {
   ssize_t stored = 0;
-  for (int i = 0; i < size && position_ < size_; i++, position_++, stored++) {
-    reinterpret_cast<char*>(buf)[position_] = mem_start_[i];
+  for (int i = 0; i < size && position_ + i < size_; i++, stored++) {
+    reinterpret_cast<uint8_t*>(buf)[i] = mem_start_[i + position_];
   }
+  position_ += stored;
   return stored;
 }
 
 ssize_t File::write(void* buf, size_t size) {
   ssize_t saved = 0;
   for (int i = 0; i < size && position_ < size_; i++, position_++, saved++) {
-    mem_start_[i] = reinterpret_cast<char*>(buf)[position_];
+    mem_start_[i] = static_cast<uint8_t*>(buf)[position_];
   }
   return saved;
 }
+
+int File::size() const { return size_; }
 
 File::~File() {
   lockf(fd_, F_ULOCK, 0);
--- src/File.h
+++ src/File.h
@@ -1,4 +1,21 @@
+/**
+ * @author Javier Padilla Pío
+ * @date 22/12/2021
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en ingeniería informática
+ * Curso: 2º
+ * Practice 6 - Simulación de DFAs
+ * Email: alu0101410463@ull.edu.es
+ * File.h: Interfaz de la clase File, encargada de gestionar la lectura y
+ *         escritura de archivos mapeados en memoria. References: Practice
+ * Revision history:
+ *                29/10/2021 - Creation (first version) of the code
+ */
+
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -18,11 +35,13 @@ class File {
 
   ssize_t write(void* buf, size_t size);
 
+  int size() const;
+
  private:
   int fd_;
   size_t size_;
-  char* mem_start_;
-  int position_;
+  uint8_t* mem_start_;
+  int position_ = 0;
 };
 
 #endif

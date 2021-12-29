783595126cd20f3c622406a3505a24a01cf19de9
jpiop <jpiopadilla@gmail.com>
2021-12-29 23:54:59 +0000

    Añadida interfaz básica para Client

---
+++ src/Client.cc
@@ -0,0 +1,7 @@
+#include "Client.h"
+
+void Client::get(){};
+void Client::list(){};
+void Client::abort(){};
+void Client::pause(){};
+void Client::resume(){};
---
+++ src/Client.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+class Client {
+ public:
+  void get();
+  void list();
+  void abort();
+  void pause();
+  void resume();
+};
+#endif

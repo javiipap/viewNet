bf6e4dbca6fcc45993dd1553443d96b6ea0c86c5
jpiop <jpiopadilla@gmail.com>
2021-12-30 04:24:58 +0000

    Añadida funcionalidad básica del cliente

--- src/Client.cc
+++ src/Client.cc
@@ -1,7 +1,32 @@
 #include "Client.h"
 
-void Client::get(){};
-void Client::list(){};
+Client::Client(){};
+
+void Client::set_up(sockaddr_in server_address) { server_address_ = server_address; }
+
+void Client::request(server_action action, std::string param) {
+  Message buffer;
+  EncodeAction(buffer, action, param);
+  socket_.send_to(buffer, server_address_);
+
+  bool end_found = false;
+
+  while (!end_found) {
+    socket_.recieve_from(buffer, server_address_);
+    for (int i = 0; i < buffer.chunk_size; i++) {
+      std::cout << buffer.text[i];
+
+      if (buffer.text[i] == 0) {
+        end_found = true;
+        std::cout << std::endl;
+        break;
+      }
+    }
+  }
+}
+
 void Client::abort(){};
+
 void Client::pause(){};
+
 void Client::resume(){};
--- src/Client.h
+++ src/Client.h
@@ -1,11 +1,22 @@
+#include "AES.h"
+#include "Message.h"
+#include "Socket.h"
+#include "functions.h"
+
 #ifndef CLIENT_H_
 #define CLIENT_H_
 class Client {
  public:
-  void get();
-  void list();
+  Client();
+  void set_up(sockaddr_in server_address);
+  void request(server_action action, std::string param = "");
   void abort();
   void pause();
   void resume();
+
+ private:
+  Socket socket_ = Socket(make_ip_address(0, "127.0.0.1"));
+  sockaddr_in server_address_;
+  AES aes_ = AES(AES::AES_256);
 };
 #endif

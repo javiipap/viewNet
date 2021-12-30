690645fcc1ab71ce536daacbd27c8cbfd58f1d66
jpiop <jpiopadilla@gmail.com>
2021-12-30 04:25:27 +0000

    Añadidas funciones para condificar y decodificar las acciones del servidor

--- src/functions.cc
+++ src/functions.cc
@@ -1,6 +1,6 @@
 #include "functions.h"
 
-sockaddr_in make_ip_address(int port, const std::string &ip_address) {
+sockaddr_in make_ip_address(int port, const std::string& ip_address) {
   in_addr address;
   if (ip_address.length())
     inet_aton(ip_address.c_str(), &address);
@@ -24,18 +24,52 @@ std::string extract_string(Message message) {
   return result;
 }
 
-std::string DecodeAction() { return ""; }
-
-std::string GenerateUid() {
-  char uid[61];
-  srand(time(NULL));
-  snprintf(uid, sizeof(uid), "%x%x-%x-%x-%x-%x%x%x", rand(), rand(), rand(),
-           ((rand() & 0x0fff) | 0x4000), rand() % 0x3fff + 0x8000, rand(), rand(), rand());
-
-  return uid;
+void EncodeAction(Message& buffer, server_action action, std::string param) {
+  for (int i = 0; i < 6; i++) {
+    buffer.text[i] = 0;
+  }
+  buffer.text[6] = action;
+  if (action ^ WITH_PARAM_ACTION < WITH_PARAM_ACTION) {
+    memcpy(buffer.text.data() + 7, param.c_str(), param.size() + 1);
+  }
 }
 
-void sigusr_1_handler(int signo, siginfo_t *info, void *context) { info->si_errno = EINTR; }
+server_action DecodeAction(const Message& buffer, std::string* param) {
+  for (int i = 0; i < 6; i++) {
+    if (buffer.text[i] == 0) {
+      continue;
+    }
+    throw std::invalid_argument("El mensaje está mal codificado");
+  }
+
+  int action = buffer.text[6];  // Admite 2^8 acciones.
+  server_action casted_action;
+
+  try {
+    casted_action = static_cast<server_action>(action);
+    if (casted_action ^ WITH_PARAM_ACTION < WITH_PARAM_ACTION) {
+      for (int i = 7; buffer.text[i] != 0; i++) {
+        *param += buffer.text[i];
+      }
+    }
+  } catch (...) {
+    throw std::invalid_argument("Acción no soportada.");
+  }
+
+  return casted_action;
+}
+
+std::string GenerateUid() {
+  std::string uuid;
+  uuid.resize(UUID_LENGTH);
+  srand(time(NULL));
+  snprintf(uuid.data(), uuid.size(), "%x%x-%x-%x-%x-%x%x%x", rand(), rand(), rand(),
+           ((rand() & 0x0fff) | 0x4000), rand() % 0x3fff + 0x8000, rand(), rand(), rand());
+
+  return uuid;
+}
+
+void sigusr_1_handler(int signo, siginfo_t* info, void* context) { info->si_errno = EINTR; }
 
 void set_signals() {
   sigset_t set;
--- src/functions.h
+++ src/functions.h
@@ -1,21 +1,37 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
+
+#define WITH_PARAM_ACTION 0x80
+#define UUID_LENGTH 0x3c
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 
 #include <iostream>
 
 #include "Message.h"
 
-struct sockaddr_in make_ip_address(int port, const std::string& ip_address = std::string());
+enum server_action {
+  list_files,
+  get_file = WITH_PARAM_ACTION | 0,
+  abortar = WITH_PARAM_ACTION | 1,
+  pausar = WITH_PARAM_ACTION | 2,
+  resume = WITH_PARAM_ACTION | 3
+};
+
+sockaddr_in make_ip_address(int port, const std::string& ip_address = std::string());
 
 std::string extract_string(Message message);
 
 bool starts_with(const std::array<char, 1024>& array, std::string prefix);
 
-std::string DecodeAction();
+void EncodeAction(Message& buffer, server_action action, std::string param = "");
+
+server_action DecodeAction(const Message& buffer, std::string* param = nullptr);
 
 std::string GenerateUid();
 
